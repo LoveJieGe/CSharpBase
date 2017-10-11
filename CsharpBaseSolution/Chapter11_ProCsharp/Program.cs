27a68edc6654e6ebec6090f013ad8ec1f1bb6b9a
songtaojie <songtaojie@aio5.com>
2017-10-11 14:00:36 +0800

    扩展方法和查询

--- CsharpBaseSolution/Chapter11_ProCsharp/Program.cs
+++ CsharpBaseSolution/Chapter11_ProCsharp/Program.cs
@@ -9,7 +9,11 @@ namespace Chapter11_ProCsharp
     {
         static void Main(string[] args)
         {
-            ProCsharp.Linq.LinqProgram.LinqQuery();
+            //ProCsharp.Linq.LinqProgram.LinqQuery();
+            //ProCsharp.Linq.LinqProgram.ExtensionMethods();
+            //ProCsharp.Linq.LinqProgram.LinqFilter();
+            //ProCsharp.Linq.LinqProgram.LinqManyFrom();
+            ProCsharp.Linq.LinqProgram.LinqGroupBy();
             Console.ReadKey();
         }
     }
