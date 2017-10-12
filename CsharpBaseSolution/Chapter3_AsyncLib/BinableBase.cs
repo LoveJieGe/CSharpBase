1a93aba28983703e9115c0bbc842ab0480bf1886
songtaojie <songtaojie@aio5.com>
2017-10-12 11:02:22 +0800

    wpf演示异步模式

---
+++ CsharpBaseSolution/Chapter3_AsyncLib/BinableBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter13_AsyncLib
+{
+    /// <summary>
+    /// 实现了通知机制
+    /// </summary>
+    public class BinableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChange(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChanged = PropertyChanged;
+            if (propertyChanged != null)
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        protected void SetProperty<T>(ref T prop,T value,[CallerMemberName] string memberName="")
+        {
+            if (!EqualityComparer<T>.Default.Equals(prop, value))
+            {
+                prop = value;
+                OnPropertyChange(memberName);
+            }
+        }
+    }
+}
