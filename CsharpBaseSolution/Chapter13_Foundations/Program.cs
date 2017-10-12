1603db03c75895434ed346ba9423cf0c2a18ad97
songtaojie <stjworkemail@163.com>
2017-10-12 22:44:23 +0800

    异步编程

---
+++ CsharpBaseSolution/Chapter13_Foundations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace Chapter13_Foundations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "胡歌";
+            //var result = Greeting(name);
+            //var result = GreetingAsync(name);
+            //Console.WriteLine(result.Result);
+            CallerWaitAsync();
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// 同步执行的方法
+        /// </summary>
+        /// <param name="name"></param>
+        static string Greeting(string name)
+        {
+            Thread.Sleep(3000);
+            return string.Format("Hello,{0}",name);
+        }
+        /// <summary>
+        /// 异步执行
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        static Task<string> GreetingAsync(string name)
+        {
+            return Task.Run<string>(()=>Greeting(name));
+        }
+        private async static void CallerWaitAsync()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            string result = await GreetingAsync("赵丽颖");
+            string result2 = await GreetingAsync("刘亦菲");
+            //var result =  GreetingAsync("赵丽颖");
+            //var result2 =  GreetingAsync("刘亦菲");
+            sw.Stop();
+            Console.WriteLine("Hello,{0}+{1},时间：{2}", result, result2,sw.Elapsed);
+        }
+    }
+}
---
+++ CsharpBaseSolution/Chapter3_AsyncLib/BingRequest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Chapter13_AsyncLib
+{
+    public class BingRequest:IImageRequest
+    {
+        private const string AppId = "请输入你的必应的AppID";
+        public BingRequest()
+        {
+            Count = 50;
+            OffSet = 0;
+        }
+        public int Count
+        { get; set; }
+        public int OffSet { get; set; }
+        private string searchTerm;
+        public string SearchTerm
+        {
+            get { return searchTerm; }
+            set { this.searchTerm = value; }
+        }
+
+        public ICredentials Credentials
+        {
+            get { return new NetworkCredential(AppId, AppId); }
+        }
+        public string Url
+        {
+            get
+            {
+                return string.Format("https://api.datamarket.azure.com/" +
+                    "Data.ashx/Bing/Search/v1/Image?Query=%27{0}%27&" +
+                    "$top={1}&$skip={2}&$format=Atom", SearchTerm, Count, OffSet);
+            }
+        }
+
+        public IEnumerable<SearchItemResult> Parse(string xml)
+        {
+            XElement respXml = XElement.Parse(xml) ;
+            XNamespace d = XNamespace.Get("http://schemas.microsoft.com/ado/2007/08/dataservices");
+            XNamespace m = XNamespace.Get("http://schemas.microsoft.com/ado/2007/08/dataservices/metadata");
+            var result = from item in respXml.Descendants(m + "properties")
+                         select new SearchItemResult()
+                         {
+                             Title = new string(item.Element(d+"Title").Value.Take(50).ToArray()),
+                             Url = item.Element(d+"MediaUrl").Value,
+                             ThumbnailUrl = item.Element(d+"Thumbnail").Element(d+"MediaUrl").Value,
+                             Source="Bing"
+                         };
+            return result;
+        }
+    }
+}
