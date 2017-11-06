1f5a75981d9b8509bb969e37dbc4ccbe5b26f3d4
stjworkemail <stjworkemail@163.com>
2017-11-06 23:04:32 +0800

    练习

---
+++ CsharpBaseSolution/Chapter21_ParallelSample/ThreadProgram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter21_ParallelSample
+{
+    public class ThreadProgram
+    {
+        public static void ThreadSample()
+        {
+            var t1 = new Thread(() =>
+            {
+                Console.WriteLine("线程1启动");
+            });
+            t1.Start();
+        }
+
+        public static void ThreadWithParameter(object o)
+        {
+            Data d = (Data)o;
+            Console.WriteLine("运行一个线程，接收参数：{0}",d.Message);
+        }
+        public static void StartThreadWithParameter()
+        {
+            Data d = new Data { Message = "这是一个参数" };
+            var t1 = new Thread(ThreadWithParameter);
+            t1.Start(d);
+        }
+    }
+
+    public struct Data
+    {
+        public string Message;
+    }
+}
---
+++ CsharpBaseSolution/Chapter27_QuoteClient/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chapter27_QuoteClient
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private QuoteInformation quoteInformation = new QuoteInformation();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = quoteInformation;
+        }
+        protected async void OnGetQuote(object sender,RoutedEventArgs e)
+        {
+            const int bufferSize = 1024;
+            Cursor cursor = this.Cursor;
+            this.Cursor = Cursors.Wait;
+            quoteInformation.EnableRequest = false;
+            string serverName = Properties.Settings.Default.ServerName;
+            int port = Properties.Settings.Default.Port;
+            var client = new TcpClient();
+            NetworkStream stream = null;
+            try
+            {
+                await client.ConnectAsync(serverName, port);
+                stream = client.GetStream();
+                byte[] buffer = new byte[bufferSize];
+                int received = await stream.ReadAsync(buffer, 0, bufferSize);
+                if(received<=0)
+                {
+                    return;
+                }
+                quoteInformation.Quote = Encoding.Unicode.GetString(buffer).Trim('\0');
+            }
+            catch(SocketException ex)
+            {
+                MessageBox.Show(ex.Message, "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                if(stream!=null)
+                {
+                    stream.Close();
+                }
+                if(client.Connected)
+                {
+                    client.Close();
+                }
+            }
+            this.Cursor = cursor;
+            quoteInformation.EnableRequest = true;
+        }
+    }
+}
