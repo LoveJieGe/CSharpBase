763deb0fcda4183f2c61d0ae7f95d2cffe9726ca
stjworkemail <stjworkemail@163.com>
2017-11-07 23:03:55 +0800

    本地化

---
+++ CsharpBaseSolution/Chapter28_CultureInfo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace Chapter28_CultureInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberFormat(1234567890);
+            Console.ReadKey();
+        }
+        private static void NumberFormat(int value)
+        {
+            //使用本地的
+            Console.WriteLine(value.ToString("N"));
+            //使用特定的
+            Console.WriteLine(value.ToString("N", new CultureInfo("fr-FR")));
+            //使用现线程
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("de-DE");
+            Console.WriteLine(value.ToString("N"));
+
+        }
+    }
+}
