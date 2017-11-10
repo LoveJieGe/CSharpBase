7e32c9aea0011018f8e35cf28164e56f546a2cf0
songtaojie <songtaojie@aio5.com>
2017-11-10 13:09:28 +0800

    webform本地化

---
+++ CsharpBaseSolution/Chapter28_ResourceDemo/Resource/ResourceFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter28_ResourceDemo.Resource
+{
+    public class ResourceFactory : IResourceFactory
+    {
+        private List<string> resxList;
+        public ResourceFactory()
+        {
+            resxList = new List<string>();
+            resxList.Add("Chapter28_ResourceDemo.Resources.Admins");
+            resxList.Add("Chapter28_ResourceDemo.Resources.Demo");
+        }
+        public string GetResource(string key, string orginal)
+        {
+            for(int i = 0;i<resxList.Count;i++)
+            {
+                string resource = ResourceHelper.GetString(resxList[i], key);
+                if (!string.IsNullOrEmpty(resource))
+                    return resource;
+            }
+            return orginal;
+        }
+    }
+}
