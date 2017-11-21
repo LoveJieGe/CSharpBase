f31eb2b9b06cd7cfd807b8f76556441735c13b84
songtaojie <songtaojie@aio5.com>
2017-11-21 16:33:30 +0800

    xml

---
+++ CsharpBaseSolution/Chapter34_XmlADO/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Chapter34_XmlADO
+{
+    public class Inventory
+    {
+        private Product[] stuff;
+        [XmlArrayItem("prod",typeof(Product)),XmlArrayItem("book",typeof(BookProduct))]
+        public Product[] InventoryItem
+        {
+            get { return this.stuff; }
+            set { this.stuff = value; }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(Product item in stuff)
+            {
+                sb.AppendFormat("ProductName:{0}\r\n", item.ProductName);
+            }
+            return sb.ToString();
+        }
+    }
+}
