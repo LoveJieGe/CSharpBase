7b76018cf461bfd19c858da07bdf49245ce6fe9c
stjworkemail <stjworkemail@163.com>
2017-12-07 23:35:31 +0800

    我吃饭改进

---
+++ CsharpBaseSolution/MessageService/IMyMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageService
+{
+    [ServiceContract(CallbackContract =typeof(IMyMessageCallBack))]
+    public interface IMyMessage
+    {
+        [OperationContract]
+        void MessageToServer(string message);
+    }
+    public interface IMyMessageCallBack
+    {
+        [OperationContract(IsOneWay =true)]
+        void OnCallBack(string message);
+    }
+}
