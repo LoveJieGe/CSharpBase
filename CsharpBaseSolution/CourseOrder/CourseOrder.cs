61e0452ebd46888b52b1954c80f63a4866661035
stjworkemail <stjworkemail@163.com>
2017-12-12 22:48:55 +0800

    创建消息队列

---
+++ CsharpBaseSolution/CourseOrder/CourseOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseOrder
+{
+    public class CourseOrder:BindableBase
+    {
+        private Customer customer;
+        public Customer Customer
+        {
+            get { return this.customer; }
+            set { SetProperty(ref this.customer, value, "Customer"); }
+        }
+        private Course course;
+        public Course Course
+        {
+            get { return this.course; }
+            set { SetProperty(ref course, value, "Course"); }
+        }
+    }
+}
